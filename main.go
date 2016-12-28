d0105190cb05b26dfbfaef25bb54bf9970cf3704
Rafal Gajdulewicz <r.gajdulewicz@gmail.com>
2016-12-28 21:41:06 +0100

    Initial version

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"net/http"
 
+	"github.com/codegangsta/negroni"
+	"github.com/gorilla/mux"
+
+	services "github.com/rafax/scari/services"
+)
+
+func main() {
+	js := services.New()
+	n := negroni.Classic()
+	r := mux.NewRouter()
+	r.HandleFunc("/jobs", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		jobs, err := js.GetAll()
+		json.NewEncoder(w).Encode(jobs)
+	})
+	r.HandleFunc("/articles", ArticlesHandler)
+	http.Handle("/", r)
+}
+
+type JobsResponse struct {
+	jobs []Job
 }
