3bed392d7f3a2bb2aa0b0284eeab21c08673c927
Rafal Gajdulewicz <r.gajdulewicz@gmail.com>
2017-01-05 20:29:19 +0100

    Drop Prometheus

--- cmd/scari-server/main.go
+++ cmd/scari-server/main.go
@@ -6,10 +6,8 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/urfave/negroni"
 
-	"github.com/prometheus/client_golang/prometheus"
 	"github.com/rafax/scari/handlers"
 	"github.com/rafax/scari/services"
-	negroniprometheus "github.com/zbindenren/negroni-prometheus"
 )
 
 func main() {
@@ -17,12 +15,8 @@ func main() {
 	n := negroni.New()
 	n.Use(negroni.NewLogger())
 	router := mux.NewRouter()
-	p := negroniprometheus.NewMiddleware("scari")
 
 	n.Use(negroni.NewRecovery())
-	n.Use(p)
-	router.Handle("/metrics", prometheus.Handler())
-
 	handlers.New(js).Register(router)
 	n.UseHandler(router)
 	http.ListenAndServe(":3001", n)
