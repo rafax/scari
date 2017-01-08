c6b7dee51e0fd8b9da8aa28ba2c34e55311e1b11
Rafal Gajdulewicz <r.gajdulewicz@gmail.com>
2017-01-08 19:13:38 +0100

    WIP on static files

--- cmd/scari-static/app.go
+++ cmd/scari-static/app.go
@@ -16,6 +16,10 @@ import (
 	"google.golang.org/appengine/file"
 )
 
+const (
+	kind = "Key"
+)
+
 func init() {
 	router := mux.NewRouter()
 	router.HandleFunc("/", homeHandler)
@@ -30,67 +34,67 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 
 func getHandler(w http.ResponseWriter, r *http.Request) {
 	v := mux.Vars(r)
-	fmt.Fprintf(w, v["fileid"])
+	id, err := strconv.ParseInt(v["fileid"], 10, 64)
+	if err != nil {
+		http.Error(w, format(err), 500)
+		return
+	}
+	ctx := appengine.NewContext(r)
+	file := new(staticFile)
+	if err = datastore.Get(ctx, datastore.NewKey(ctx, kind, "", id, nil), file); err != nil {
+		http.Error(w, format(err), 500)
+		return
+	}
+	fmt.Fprintf(w, file.StorageURL)
 }
 
 func postHandler(w http.ResponseWriter, r *http.Request) {
 	var sfr StaticFileRequest
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		w.WriteHeader(500)
-		fmt.Fprintf(w, "{\"error\":\"%v\"}", err)
+		http.Error(w, format(err), 500)
 		return
 	}
 	err = json.Unmarshal(body, &sfr)
 	if err != nil {
-		w.WriteHeader(500)
-		fmt.Fprintf(w, "{\"error\":\"%v\"}", err)
+		http.Error(w, format(err), 500)
 		return
 	}
 	ctx := appengine.NewContext(r)
 	client, err := storage.NewClient(ctx)
 	if err != nil {
-		w.WriteHeader(500)
-		fmt.Fprintf(w, "{\"error\":\"%v\"}", err)
+		http.Error(w, format(err), 500)
 		return
 	}
 	bucketName, err := file.DefaultBucketName(ctx)
 	if err != nil {
-		w.WriteHeader(500)
-		fmt.Fprintf(w, "{\"error\":\"%v\"}", err)
+		http.Error(w, format(err), 500)
 		return
 	}
 	bucket := client.Bucket(bucketName)
 	_, err = bucket.Object(sfr.FileName).Attrs(ctx)
 	if err != nil {
-		w.WriteHeader(500)
-		fmt.Fprintf(w, "{\"error\":\"%v\"}", err)
+		http.Error(w, format(err), 500)
 		return
 	}
-	key := datastore.NewIncompleteKey(ctx, "File", nil)
+	key := datastore.NewIncompleteKey(ctx, kind, nil)
 	file := staticFile{
 		FileName:   sfr.FileName,
 		StorageURL: fmt.Sprintf("https://storage.googleapis.com/%s/%s", scari.StorageBucketName, sfr.FileName),
 	}
 	k, err := datastore.Put(ctx, key, &file)
 	if err != nil {
-		w.WriteHeader(500)
-		fmt.Fprintf(w, "{\"error\":\"%v\"}", err)
+		http.Error(w, format(err), 500)
 		return
 	}
 	id := strconv.FormatInt(k.IntID(), 10)
 	resp, err := json.Marshal(StaticFileResponse{Id: id})
 	if err != nil {
-		w.WriteHeader(500)
-		fmt.Fprintf(w, "{\"error\":\"%v\"}", err)
+		http.Error(w, format(err), 500)
 		return
 	}
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(resp)
-}
-
-func checkErr(err error, w http.ResponseWriter) {
-
 }
 
 type StaticFileRequest struct {
@@ -104,4 +108,8 @@ type StaticFileResponse struct {
 type staticFile struct {
 	FileName   string
 	StorageURL string
+}
+
+func format(err error) string {
+	return fmt.Sprintf("{\"error\":\"%v\"}", err)
 }
