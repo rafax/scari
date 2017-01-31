3a16ecc12daecbc73a12b8607f75077a93e2c458
Rafal Gajdulewicz <r.gajdulewicz@gmail.com>
2017-01-31 21:50:58 +0100

    Add acceptance tests

---
+++ acceptance_tests/lib/acceptance_tests.ex
@@ -0,0 +1,18 @@
+defmodule AcceptanceTests do
+  @moduledoc """
+  Documentation for AcceptanceTests.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AcceptanceTests.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
